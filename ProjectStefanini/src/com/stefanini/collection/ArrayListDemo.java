0b8a3d8807354af6241863bf05357941d5f3cae8
Aniket Pantawane <pantawaneaniket@gmail.com>
2024-12-31 15:50:42 +0530

    changed to for each implementation

--- ProjectStefanini/src/com/stefanini/collection/ArrayListDemo.java
+++ ProjectStefanini/src/com/stefanini/collection/ArrayListDemo.java
@@ -1,7 +1,7 @@
 package com.stefanini.collection;
 
 import java.util.Arrays;
-import java.util.Iterator;
+
 import java.util.List;
 
 public class ArrayListDemo {
@@ -9,12 +9,11 @@ public class ArrayListDemo {
 	public static void main(String[] args) {
 
 		List<String> list = Arrays.asList("Sagar", "Sawant", "Sakshi", "Sumit", "Suraj");
-		Iterator<String> itr = list.iterator();
-
-		while (itr.hasNext()) {
-			System.out.println(itr.next());
-		}
 		
+
+	for (String str : list) {
+		System.out.println(str);
+	}
 
 	}
 	
